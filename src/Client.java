5bfcdabb74ad0c812bc0d3d1ac8db4c8c528ad75
rrg2504@g.rit.edu <Ramsesisthe2899>
2023-08-18 00:23:57 -0400

    example client

---
+++ src/Client.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        String hostname = "192.168.1.127";
+        int port = 6868;
+
+        try (Socket socket = new Socket(hostname, port)) {
+
+            InputStream input = socket.getInputStream();
+            InputStreamReader reader = new InputStreamReader(input);
+
+            int character;
+            StringBuilder data = new StringBuilder();
+
+            while ((character = reader.read()) != -1) {
+                data.append((char) character);
+            }
+
+            System.out.println(data);
+
+
+        } catch (UnknownHostException ex) {
+
+            System.out.println("Server not found: " + ex.getMessage());
+
+        } catch (IOException ex) {
+
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+}
