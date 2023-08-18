4452433d965228b510a2281df9bafe7b3bfcf083
rrg2504@g.rit.edu <Ramsesisthe2899>
2023-08-18 00:19:07 -0400

    example host socket class

--- src/Host.java
+++ src/Host.java
@@ -1,7 +1,31 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class Host {
     int test = 0;
 
     public static void main(String[] args){
-        System.out.println("hello world");
+        int port = 6868;
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                System.out.println("New client connected");
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+                writer.println(new Date().toString());
+            }
+        } catch (IOException ex) {
+            System.out.println("Server Exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 }
