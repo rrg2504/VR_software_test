3d44dfacb4cc4ccb1dc5f8940b4bc099ef66b4f1
rrg2504@g.rit.edu <Ramsesisthe2899>
2023-08-19 17:02:54 -0400

    updated intellij

--- src/Client.java
+++ src/Client.java
@@ -18,7 +18,7 @@ public class Client {
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
-            //close everything
+            closeEverything(socket,bufferedReader,bufferedWriter);
         }
     }
 
